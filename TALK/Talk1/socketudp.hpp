752d18c47f2efc8e1853369e22583e2033f31403
Alejandro Delgado <alu0100767452@ull.edu.es>
2016-02-20 18:30:21 +0000

    Creando estructura de la clase Socket mediante una clase

---
+++ TALK/Talk1/socketudp.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <cstring>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+struct Message{
+    char text[1024];
+};
+
+class Socket{
+
+    private:
+        int fd;
+
+    public:
+        Socket();
+        ~Socket();
+        void send_to(const Message& message, const sockaddr_in& address);
+        void receive_from(Message& message_, const sockaddr_in& address);
+
+
+};
+
+sockaddr_in make_ip_address( std::string ip_address, int port );
